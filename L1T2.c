8249be51f733a0a0b6e169902b36a006c1ee22ae
perttipenttinen <31660385+perttipenttinen@users.noreply.github.com>
2025-06-02 21:42:20 +0300

    Update L1T2.c

--- L1T2.c
+++ L1T2.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 int main(void){
+  int kokonaisluku;
+  float liukuluku;
   printf("Anna kokonaisluku: ");
   scanf("%d", &kokonaisluku);
   printf("Anna liukuluku: ");
   scanf("%2f", &liukuluku);
-  print("Annoit luvut ",kokonaisluku," ja ",liukuluku);
+  print("Annoit luvut &kokonaisluku ja %liukuluku",".", kokonaisluku,liukuluku);
 }
