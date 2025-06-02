5154603efd1fabdff0a03a5a3ef858387eae5e00
perttipenttinen <31660385+perttipenttinen@users.noreply.github.com>
2025-06-02 21:37:25 +0300

    Create L1T2.c

---
+++ L1T2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void){
+  printf("Anna kokonaisluku: ");
+  scanf("%d", &kokonaisluku);
+  printf("Anna liukuluku: ");
+  scanf("%2f", &liukuluku);
+  print("Annoit luvut ",kokonaisluku," ja ",liukuluku);
+}
